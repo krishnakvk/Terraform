2bc9b1803286c8ce4f4381e175d52d827caa9d73
vamshikrishna <vamshi.krishna@redmorph.com>
2019-12-04 18:26:55 +0530

    ebs creation

--- ec2/main.tf
+++ ec2/main.tf
@@ -3,6 +3,7 @@ provider "aws" {
 }
 data "aws_availability_zones" "avz" {}
 
+#Getting latest cento os AMI id. To know how to get owner id see the 100days devops documentaion
 data "aws_ami" "my_centos" {
   owners = [679593333241]
   most_recent = true
@@ -20,16 +21,18 @@ data "aws_ami" "my_centos" {
   }
 }
 
+#Creating key pair
 resource "aws_key_pair" "My_keypair" {
   key_name = "Terraform_keypair"
   public_key = "${file(var.my_public_key)}"
-  /*public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDdqg1ajFNpBwoTct4CYFF9/d/i9hLTOowtUH6Cxd+nzmQLgE+1x0m3KdR0O4uSS/k63lJg5MYMbXeQGXxm2ivoZhnjIe5W0RjXLTC+3dDSrmnbodhXdmQ2C0j7Jr/mHVhB0JKzhfZrLzfOKeAGdVULH4gjWLK1D1ryPBCoF4b837ky2mxLBZAQY5YO+AD6Qu12DAfamq6gC0ZAYB6Nuq+itGrnwutPmivUttZIz9WxjrmtlX17yyzkHEb+TXP1gPbZedU1QsTkFkSIIh9qPg9sOUuPW3b6bpD4qixLF6SCvGhnbVunlb+0cPC6aR3gxDBY1FA0txgZiwCPQNESPrun vamshi@vamshi-Inspiron-5558o"*/
+  /*public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDdqg1ajFNpBwoTct4CYFF9/d/i9hLTOowtUH6Cxd+nzmQLgE+1x0m3KdR0O4uSS/k63lJg5MYMbXeQGXxm2ivoZhnjIe5W0RjXLTC+3dDSrmnbodhXdmQ2C0j7Jr/mHVhB0JKzhfZrLzfOKeAGdVULH4gjWLK1D1ryPBCoF4b837ky2mxLBZAQY5YO+AD6Qu12DAfamq6gC0ZAYB6Nuq+itGrnwutPmivUttZIz9WxjrmtlX17yyzkHEb+TXP1gPbZedU1QsTkFkSIIh9qPg9sOUuPW3b6bpD4qixLF6SCvGhnbVunlb+0cPC6aR3gxDBY1FA0txgZiwCPQNESPrun redmorph@redmorph-Inspiron-5558o"*/
 }
-
+#Defining user data file
 data "template_file" "init" {
   template = file("${path.module}/userdata.tpl")
 }
 
+#creating Instance
 resource "aws_instance" "web" {
   count = 2
   ami = data.aws_ami.my_centos.id
@@ -44,5 +47,20 @@ resource "aws_instance" "web" {
     Name = "my-instance-${count.index + 1}"
   }
 }
+# Creating Volumes
+resource "aws_ebs_volume" "volume" {
+  count = 2
+  availability_zone = data.aws_availability_zones.avz.names[count.index]
+  size = 1
+  type = "gp2"
+}
 
+#Attaching volumes to isnatnces
+#Type is gp2(other available options "standard", "gp2", "io1", "sc1" or "st1" (Default: "standard"))
+resource "aws_volume_attachment" "my-vol-attach" {
+  count = 2
+  device_name = "/dev/xvdh"
+  instance_id = aws_instance.web.*.id[count.index]
+  volume_id = aws_ebs_volume.volume.*.id[count.index]
+}
 
--- ec2/userdata.tpl
+++ ec2/userdata.tpl
@@ -1,4 +1,8 @@
 #!/bin/bash
+mkfs.ext4 /dev/xvdh
+mount /dev/xvdh /mnt
+echo /dev/xvdh /mnt defaults,nofail 0 2 >> /etc/fstab
+
 yum update -y
 yum install httpd -y
 echo "my terraform code" >> /var/www/html/index.html
